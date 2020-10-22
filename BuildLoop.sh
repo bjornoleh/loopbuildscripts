758b89f8e692c7f5f27bb16426a2b1318e7af54e
jonfawcett <38429455+jonfawcett@users.noreply.github.com>
2020-10-22 19:54:33 -0400

    Update BuildLoop.sh

--- BuildLoop.sh
+++ BuildLoop.sh
@@ -1,13 +1,13 @@
 Echo     Set environment variables
 LOOP_BUILD=$(date +'%y%m%d-%H%M')
-LOOP_DIR=~/Downloads/BuildLoop/Loop-$LOOP_BUILD
+LOOP_DIR=~/Downloads/BuildLoop/FreeAPS-$LOOP_BUILD
 Echo make directories using format year month date hour minute so it can be easily sorted
 mkdir ~/Downloads/BuildLoop/
 mkdir $LOOP_DIR
 cd $LOOP_DIR
 pwd
 Echo download software from github
-git clone --branch=master --recurse-submodules https://github.com/LoopKit/LoopWorkspace
+git clone --branch=freeaps --recurse-submodules https://github.com/ivalkou/LoopWorkspace
 cd LoopWorkspace
 Echo Open xcode
 xed .
